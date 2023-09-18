ff1ea23d072d546115f5291afd89f65e62579e80
sushamaa <89633663+sushamaa@users.noreply.github.com>
2023-09-18 20:47:21 +0530

    Update site.py

--- site.py
+++ site.py
@@ -279,4 +279,7 @@ def graph1(the_month, export, the_region):
 
 graph1(the_month, export, the_region)
 
-
+# Add a disclaimer
+st.markdown("---")
+st.markdown("#### Disclaimer: The data presented in this document or any associated content is entirely fictional and for illustrative purposes only. Any resemblance to real individuals, organizations, or events is purely coincidental. This data should not be used for any real-world decisions, analysis, or actions, as it is entirely fabricated and does not reflect actual information or circumstances.")
+st.markdown("---")
